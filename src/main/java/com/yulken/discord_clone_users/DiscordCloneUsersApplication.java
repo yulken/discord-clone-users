b793f9ad8b5fae180df5af7d5dec156d64f2f5d8
Caio Torres <caiocftorres@gmail.com>
2025-03-13 20:00:25 -0300

    feat: first commit

---
+++ src/main/java/com/yulken/discord_clone_users/DiscordCloneUsersApplication.java
@@ -0,0 +1,13 @@
+package com.yulken.discord_clone_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordCloneUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordCloneUsersApplication.class, args);
+	}
+
+}
